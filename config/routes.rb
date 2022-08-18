745376e9f5d7558b48be56eaf4a67a12cb4e7ecf
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-18 03:03:17 -0500

    add form to the garden show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :gardens
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :gardens do
+    resources :plants, only: :create
+  end
+
+  # "/gardens/:garden_id/plants"
 end
