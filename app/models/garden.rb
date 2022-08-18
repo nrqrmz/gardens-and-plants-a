87cc0f12dbd2bda9241fbc4fb08a83ae76386629
Enrique Ramirez <nrqrmz@gmail.com>
2022-08-18 02:41:48 -0500

    add plant and garden validations

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,6 @@
-class Garden < ApplicationRecord
+class Garden <
+  has_many :plants, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
   validates :banner_url, presence: true
 end
